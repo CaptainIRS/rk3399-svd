7145f2deb994d04e7095ca76760868cf187b0045
CaptainIRS <36656347+CaptainIRS@users.noreply.github.com>
2024-05-01 15:57:49 +0530

    Include more peripherals

--- create_svd.py
+++ create_svd.py
@@ -4,6 +4,7 @@ import re
 
 part1 = json.load(open('part1_final.json'))
 part2 = json.load(open('part2_final.json'))
+part3 = json.load(open('part3_final.json'))
 
 ddr = json.load(open('ddr.json'))
 hdmi = json.load(open('hdmi/regs.json'))
@@ -11,7 +12,7 @@ displayport = json.load(open('displayport/regs.json'))
 pcie = json.load(open('pcie/regs.json'))
 
 attrs = []
-for group in part1 + part2:
+for group in part1 + part2 + part3:
     for register in group['registers']:
         for bit_range in register['bit_ranges']:
             attrs.append(bit_range['attr'].replace('\n', ''))
@@ -610,11 +611,13 @@ device_maps = {
         {"name": "MMU1_ISP0", "base": "0xff915000", "description": "Registers of Memory Management Unit 1 (MMU1) for Image Signal Processor 0 (ISP0)"},
         {"name": "MMU0_ISP1", "base": "0xff924000", "description": "Registers of Memory Management Unit 0 (MMU0) for Image Signal Processor 1 (ISP1)"},
         {"name": "MMU1_ISP1", "base": "0xff925000", "description": "Registers of Memory Management Unit 1 (MMU1) for Image Signal Processor 1 (ISP1)"},
-        {"name": "MMU_VOPB", "base": "0xff900300", "description": "Registers of Memory Management Unit (MMU) for Video Output Processor (Big) (VOPB)"},
-        {"name": "MMU_VOPL", "base": "0xff8f0300", "description": "Registers of Memory Management Unit (MMU) for Video Output Processor (Little) (VOPL)"},
+        {"name": "MMU_VOPB", "base": "0xff903f00", "description": "Registers of Memory Management Unit (MMU) for Visual Output Processor (Big) (VOPB)"},
+        {"name": "MMU_VOPL", "base": "0xff8f3f00", "description": "Registers of Memory Management Unit (MMU) for Visual Output Processor (Little) (VOPL)"},
         {"name": "MMU_IEP", "base": "0xff670800", "description": "Registers of Memory Management Unit (MMU) for Image Enhancement Processor (IEP)"},
         {"name": "MMU_HDCP", "base": "0xff930000", "description": "Registers of Memory Management Unit (MMU) for High-bandwidth Digital Content Protection (HDCP)"},
-
+        {"name": "MMU_RKVDEC_R", "base": "0xff660480", "description": "Registers of Memory Management Unit (MMU) for Rockchip Video Decoder (RKVDEC) Read"},
+        {"name": "MMU_RKVDEC_W", "base": "0xff6604c0", "description": "Registers of Memory Management Unit (MMU) for Rockchip Video Decoder (RKVDEC) Write"},
+        {"name": "MMU_VPU", "base": "0xff650800", "description": "Registers of Memory Management Unit (MMU) for Video Processing Unit (VPU)"},
     ],
     "CCI500": [
         {"name": "CCI500", "base": "0xFFB00000", "description": "Cache Coherent Interconnect 500 (CCI500) Registers"},
@@ -678,6 +681,43 @@ device_maps = {
     "SPDIF": [
         {"name": "SPDIF", "base": "0xFF870000", "description": "Sony/Philips Digital Interface (SPDIF) Registers"},
     ],
+    "RKVDEC": [
+        {"name": "RKVDEC", "base": "0xFF660000", "description": "Rockchip Video Decoder (RKVDEC) Registers"},
+    ],
+    "VEPU": [
+        {"name": "VEPU", "base": "0xFF650000", "description": "Video Processor Unit (VPU) Encoder Registers"},
+    ],
+    "VDPU": [
+        {"name": "VDPU", "base": "0xFF650400", "description": "Video Processor Unit (VPU) Decoder Registers"},
+    ],
+    "PREF_CACHE": [
+        {"name": "PREF_CACHE_RKVDEC_LUMA", "base": "0xFF660400", "description": "RKVDEC Luma Prefetch Cache Control Registers"},
+        {"name": "PREF_CACHE_RKVDEC_CHROMA", "base": "0xFF660440", "description": "RKVDEC Chroma Prefetch Cache Control Registers"},
+        {"name": "PREF_CACHE_VPU", "base": "0xFF650C00", "description": "VPU Prefetch Cache Control Registers"},
+    ],
+    "RGA2": [
+        {"name": "RGA2", "base": "0xFF680000", "description": "Rockchip Graphics Accelerator 2 (RGA2) Registers"},
+    ],
+    "MIPI_DSI_HOST": [
+        {"name": "MIPI_DSI_HOST0", "base": "0xFF960000", "description": "MIPI Display Serial Interface (DSI) Host 0 Registers"},
+        {"name": "MIPI_DSI_HOST1", "base": "0xFF968000", "description": "MIPI Display Serial Interface (DSI) Host 1 Registers"},
+    ],
+    "DCF": [
+        {"name": "DCF", "base": "0xFF6A0000", "description": "DDR Converser of Frequency (DCF) Registers"},
+    ],
+    "CRYPTO": [
+        {"name": "CRYPTO0", "base": "0xFF8B0000", "description": "Crypto 0 Registers"},
+        {"name": "CRYPTO1", "base": "0xFF8B8000", "description": "Crypto 1 Registers"},
+    ],
+    "VOPB": [
+        {"name": "VOPB", "base": "0xFF900000", "description": "Visual Output Processor (Big) (VOPB) Registers"},
+    ],
+    "VOPL": [
+        {"name": "VOPL", "base": "0xFF8F0000", "description": "Visual Output Processor (Little) (VOPL) Registers"},
+    ],
+    "IEP": [
+        {"name": "IEP", "base": "0xFF670000", "description": "Image Enhancement Processor (IEP) Registers"},
+    ]
 }
 
 group_descriptions = {
@@ -715,6 +755,17 @@ group_descriptions = {
     "SARADC": "Successive Approximation Register Analog-to-Digital Converter (SARADC) Registers",
     "PWM": "Pulse Width Modulation (PWM) Registers",
     "SPDIF": "Sony/Philips Digital Interface (SPDIF) Registers",
+    "RKVDEC": "Rockchip Video Decoder (RKVDEC) Registers",
+    "VEPU": "Video Processor Unit (VPU) Encoder Registers",
+    "VDPU": "Video Processor Unit (VPU) Decoder Registers",
+    "PREF_CACHE": "VPU Prefetch Cache Registers",
+    "RGA2": "Rockchip Graphics Accelerator 2 (RGA2) Registers",
+    "MIPI_DSI_HOST": "MIPI Display Serial Interface (DSI) Host Registers",
+    "DCF": "DDR Converser of Frequency (DCF) Registers",
+    "CRYPTO": "Crypto Registers",
+    "VOPB": "Visual Output Processor (Big) (VOPB) Registers",
+    "VOPL": "Visual Output Processor (Little) (VOPL) Registers",
+    "IEP": "Image Enhancement Processor (IEP) Registers",
 }
 
 group_block_sizes = {
@@ -741,7 +792,7 @@ group_block_sizes = {
     "PROBE": "0x400",
     "DMAC": "0x10000",
     "TSADC": "0x10000",
-    "MMU": "0x30",
+    "MMU": "0x40",
     "CCI500": "0x100000",
     "TIMER": "0x20",
     "PCIE_CLIENT": "0x800000",
@@ -752,6 +803,17 @@ group_block_sizes = {
     "SARADC": "0x10000",
     "PWM": "0x10000",
     "SPDIF": "0x10000",
+    "RKVDEC": "0x400",
+    "VEPU": "0x400",
+    "VDPU": "0x400",
+    "PREF_CACHE": "0x40",
+    "RGA2": "0x10000",
+    "MIPI_DSI_HOST": "0x8000",
+    "DCF": "0x10000",
+    "CRYPTO": "0x8000",
+    "VOPB": "0x3000",
+    "VOPL": "0x3000",
+    "IEP": "0x800",
 }
 
 # (access, modifiedWriteValues, readAction)
@@ -777,7 +839,7 @@ svd_access_map = {
 
 peripherals = {}
 
-for group in part1 + part2:
+for group in part1 + part2 + part3:
     device_map_groups = device_maps[group['name']]
     base_group = device_map_groups[0]
     if group['name'] not in peripherals:
@@ -1211,10 +1273,12 @@ ET.SubElement(cpu, 'deviceNumInterrupts').text = '172'
 
 peripherals_element = ET.SubElement(root, 'peripherals')
 interrupts = json.load(open('interrupts.json'))
-peripherals = {p: x for p, x in sorted(peripherals.items(), key=lambda x: x[0])}
+# peripherals = {p: x for p, x in sorted(peripherals.items(), key=lambda x: 'TIMERS' + x[0] if x[0].startswith('PMUTIMER') or x[0].startswith('STIMER') else x[0])}
 
-ordered = ['CCI500', 'CRU', 'DDRC', 'DDRC0', 'DDRC1', 'DDR_CIC', 'DDR_MON', 'DMAC', 'DMAC0', 'DMAC1', 'DP', 'EFUSE', 'EFUSE0', 'EFUSE1', 'EMMCCORE', 'ERR_LOGGER_MSCH', 'ERR_LOGGER_MSCH0', 'ERR_LOGGER_MSCH1', 'ERR_LOGGER_SLV', 'ERR_LOGGER_SLV0', 'ERR_LOGGER_SLV1', 'GMAC', 'GPIO', 'GPIO0', 'GPIO1', 'GPIO2', 'GPIO3', 'GPIO4', 'GRF', 'HDMI', 'I2S', 'I2S0', 'I2S1', 'I2S2', 'MAILBOX', 'MAILBOX0', 'MAILBOX1', 'MMU', 'MMU0_ISP0', 'MMU0_ISP1', 'MMU1_ISP0', 'MMU1_ISP1', 'MMU_HDCP', 'MMU_IEP', 'MMU_VOPB', 'MMU_VOPL', 'MSCH', 'MSCH0', 'MSCH1', 'PCIE_CLIENT', 'PCIE_CORE', 'PMU', 'PMUCRU', 'PMUGRF', 'PROBE', 'PROBE_CCI_MSCH0', 'PROBE_CCI_MSCH1', 'PROBE_GPU_MSCH0', 'PROBE_GPU_MSCH1', 'PROBE_PERIHP_MSCH0', 'PROBE_PERIHP_MSCH1', 'PROBE_PERILP_MSCH0', 'PROBE_PERILP_MSCH1', 'PROBE_VIDEO_MSCH0', 'PROBE_VIDEO_MSCH1', 'PROBE_VIO0_MSCH0', 'PROBE_VIO0_MSCH1', 'PROBE_VIO1_MSCH0', 'PROBE_VIO1_MSCH1', 'PWM', 'QOS', 'QOS_CCI_M0', 'QOS_CCI_M1', 'QOS_CRYPTO0', 'QOS_CRYPTO1', 'QOS_DCF', 'QOS_DMAC0', 'QOS_DMAC1', 'QOS_EMMC', 'QOS_GIC', 'QOS_GMAC', 'QOS_GPU', 'QOS_HDCP', 'QOS_HSIC', 'QOS_IEP', 'QOS_ISP0_M0', 'QOS_ISP0_M1', 'QOS_ISP1_M0', 'QOS_ISP1_M1', 'QOS_PCIE', 'QOS_PERIHP_NSP', 'QOS_PERILPSLV_NSP', 'QOS_PERILP_NSP', 'QOS_PERI_CM0', 'QOS_PMU_CM0', 'QOS_RGA_R', 'QOS_RGA_W', 'QOS_SDIO', 'QOS_SDMMC', 'QOS_USB_HOST0', 'QOS_USB_HOST1', 'QOS_USB_OTG0', 'QOS_USB_OTG1', 'QOS_VIDEO_M0', 'QOS_VIDEO_M1_R', 'QOS_VIDEO_M1_W', 'QOS_VOP_BIG_R', 'QOS_VOP_BIG_W', 'QOS_VOP_LITTLE', 'RKI2C', 'RKI2C0', 'RKI2C1', 'RKI2C2', 'RKI2C3', 'RKI2C4', 'RKI2C5', 'RKI2C6', 'RKI2C7', 'RKI2C8', 'SARADC', 'SDMMC', 'SPDIF', 'SPI', 'SPI0', 'SPI1', 'SPI2', 'SPI3', 'SPI4', 'SPI5', 'TIMER', 'TIMER0', 'TIMER1', 'TIMER2', 'TIMER3', 'TIMER4', 'TIMER5', 'TIMER6', 'TIMER7', 'TIMER8', 'TIMER9', 'TIMER10', 'TIMER11', 'STIMER0', 'STIMER1', 'STIMER2', 'STIMER3', 'STIMER4', 'STIMER5', 'STIMER6', 'STIMER7', 'STIMER8', 'STIMER9', 'STIMER10', 'STIMER11', 'PMUTIMER0', 'PMUTIMER1', 'TSADC', 'UART', 'UART0', 'UART1', 'UART2', 'UART3', 'UART4', 'USB3', 'USB3_OTG0', 'USB3_OTG1', 'WDT', 'WDT0', 'WDT1', 'WDT2']
-peripherals = {p: x for p, x in sorted(peripherals.items(), key=lambda x: ordered.index(x[0]))}
+# ordered = ['CCI500', 'CRU', 'DDRC', 'DDRC0', 'DDRC1', 'DDR_CIC', 'DDR_MON', 'DMAC', 'DMAC0', 'DMAC1', 'DP', 'EFUSE', 'EFUSE0', 'EFUSE1', 'EMMCCORE', 'ERR_LOGGER_MSCH', 'ERR_LOGGER_MSCH0', 'ERR_LOGGER_MSCH1', 'ERR_LOGGER_SLV', 'ERR_LOGGER_SLV0', 'ERR_LOGGER_SLV1', 'GMAC', 'GPIO', 'GPIO0', 'GPIO1', 'GPIO2', 'GPIO3', 'GPIO4', 'GRF', 'HDMI', 'I2S', 'I2S0', 'I2S1', 'I2S2', 'MAILBOX', 'MAILBOX0', 'MAILBOX1', 'MMU', 'MMU0_ISP0', 'MMU0_ISP1', 'MMU1_ISP0', 'MMU1_ISP1', 'MMU_HDCP', 'MMU_IEP', 'MMU_VOPB', 'MMU_VOPL', 'MSCH', 'MSCH0', 'MSCH1', 'PCIE_CLIENT', 'PCIE_CORE', 'PMU', 'PMUCRU', 'PMUGRF', 'PROBE', 'PROBE_CCI_MSCH0', 'PROBE_CCI_MSCH1', 'PROBE_GPU_MSCH0', 'PROBE_GPU_MSCH1', 'PROBE_PERIHP_MSCH0', 'PROBE_PERIHP_MSCH1', 'PROBE_PERILP_MSCH0', 'PROBE_PERILP_MSCH1', 'PROBE_VIDEO_MSCH0', 'PROBE_VIDEO_MSCH1', 'PROBE_VIO0_MSCH0', 'PROBE_VIO0_MSCH1', 'PROBE_VIO1_MSCH0', 'PROBE_VIO1_MSCH1', 'PWM', 'QOS', 'QOS_CCI_M0', 'QOS_CCI_M1', 'QOS_CRYPTO0', 'QOS_CRYPTO1', 'QOS_DCF', 'QOS_DMAC0', 'QOS_DMAC1', 'QOS_EMMC', 'QOS_GIC', 'QOS_GMAC', 'QOS_GPU', 'QOS_HDCP', 'QOS_HSIC', 'QOS_IEP', 'QOS_ISP0_M0', 'QOS_ISP0_M1', 'QOS_ISP1_M0', 'QOS_ISP1_M1', 'QOS_PCIE', 'QOS_PERIHP_NSP', 'QOS_PERILPSLV_NSP', 'QOS_PERILP_NSP', 'QOS_PERI_CM0', 'QOS_PMU_CM0', 'QOS_RGA_R', 'QOS_RGA_W', 'QOS_SDIO', 'QOS_SDMMC', 'QOS_USB_HOST0', 'QOS_USB_HOST1', 'QOS_USB_OTG0', 'QOS_USB_OTG1', 'QOS_VIDEO_M0', 'QOS_VIDEO_M1_R', 'QOS_VIDEO_M1_W', 'QOS_VOP_BIG_R', 'QOS_VOP_BIG_W', 'QOS_VOP_LITTLE', 'RKI2C', 'RKI2C0', 'RKI2C1', 'RKI2C2', 'RKI2C3', 'RKI2C4', 'RKI2C5', 'RKI2C6', 'RKI2C7', 'RKI2C8', 'SARADC', 'SDMMC', 'SPDIF', 'SPI', 'SPI0', 'SPI1', 'SPI2', 'SPI3', 'SPI4', 'SPI5', 'TIMER', 'TIMER0', 'TIMER1', 'TIMER2', 'TIMER3', 'TIMER4', 'TIMER5', 'TIMER6', 'TIMER7', 'TIMER8', 'TIMER9', 'TIMER10', 'TIMER11', 'STIMER0', 'STIMER1', 'STIMER2', 'STIMER3', 'STIMER4', 'STIMER5', 'STIMER6', 'STIMER7', 'STIMER8', 'STIMER9', 'STIMER10', 'STIMER11', 'PMUTIMER0', 'PMUTIMER1', 'TSADC', 'UART', 'UART0', 'UART1', 'UART2', 'UART3', 'UART4', 'USB3', 'USB3_OTG0', 'USB3_OTG1', 'WDT', 'WDT0', 'WDT1', 'WDT2']
+# print(ordered)
+# print(list(sorted(peripherals.keys(), key=lambda x: 'TIMER' + x if 'STIMER' in x or 'PMUTIMER' in x else x)))
+peripherals = {p: x for p, x in sorted(peripherals.items(), key=lambda x: 'TIMER' + x[0] if 'STIMER' in x[0] or 'PMUTIMER' in x[0] else x[0])}
 for peripheral in peripherals:
     if 'derivedFrom' in peripherals[peripheral]:
         peripheral_element = ET.SubElement(peripherals_element, 'peripheral', derivedFrom=peripherals[peripheral]['derivedFrom'])
@@ -1291,7 +1355,7 @@ for peripheral in peripherals:
                         else:
                             non_enums.append(line)
                 # enum_exclusions = ['DP_TEST', 'TX_COMMON', 'RKI2C_ST', 'SDMMC_HCON', 'USB3_DSTS', 'USB3_GCTL', 'USB3_DCTL', 'CCI500_ERROR_STATUS', 'SDMMC_STATUS', 'EMMCCORE_CLKCTRL', 'DP_TX_VERSION']
-                field_exclusions = ['HCON', 'SCALEDOWN', 'ULSTCHNGREQ', 'USBLNKST', 'SCL_ST', 'SDA_ST', 'RESISTOR_CTRL', 'SDCLKFREQSEL', 'DP_TX_VERSION']
+                field_exclusions = ['HCON', 'SCALEDOWN', 'ULSTCHNGREQ', 'USBLNKST', 'SCL_ST', 'SDA_ST', 'RESISTOR_CTRL', 'SDCLKFREQSEL', 'DP_TX_VERSION', 'SW_H264ORVP9_ERROR_MODE', 'SW_YUV_CONV_RANGE', 'SW_PP_IN_CRBF_EN', 'WIN1_CSC_MODE', 'WIN0_CSC_MODE', 'WIN0_DATA_FMT']
                 register_exclusions = ['DP_TEST']
                 if len(enums) >= 2 and '......' not in field['description'] and field['name'] not in field_exclusions and register['name'] not in register_exclusions:
                     field['description'] = '\n'.join(non_enums)
@@ -1363,7 +1427,7 @@ for peripheral in peripherals:
                         else:
                             enum_names.append('d' + enum_value)
                     if len(enum_decs) != len(set(enum_decs)):
-                        print(register['name'], field['name'], enum_names)
+                        print('Duplicates', register['name'], field['name'], enum_names)
                     enumerated_values_element = ET.SubElement(field_element, 'enumeratedValues')
                     for i in range(len(enums)):
                         name, value, dec, description = enum_names[i], enum_values[i], enum_decs[i], enum_descriptions[i]
